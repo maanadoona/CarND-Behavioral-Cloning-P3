4dc8725fa4affb94b7439255cf1371f0225b2b38
maanadoona <maanadoona@gmail.com>
2018-08-01 20:57:26 +0000

    this code is the longest driving until now.

--- clone_my.py
+++ clone_my.py
@@ -10,7 +10,7 @@ with open('../data/driving_log.csv') as csvfile:
 
 images = []
 measurements = []
-correction = 0.01
+correction = 0.000001
 for line in lines:
 	for i in range(3):
 		source_path = line[i]
@@ -36,17 +36,38 @@ from keras.layers.core import Lambda
 from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
 
-model = Sequential()
 X_train = np.array(X_train / 255.0 - 0.5)
-#model.add(Cropping2D(cropping=((70, 25), (0, 0)), input_shape=(160, 320, 3)))
-#model.add(Convolution2D(24, 5, 5, subsample=(2, 2), activation="relu", input_shape=(160, 320, 3)))
+
+
+#my model
+model = Sequential()
 model.add(Cropping2D(cropping=((70, 25), (0, 0)), input_shape=(160, 320, 3)))
-model.add(Convolution2D(32, 5, 5, subsample=(2, 2), activation="relu"))
+model.add(Convolution2D(24, 5, 5, subsample=(2, 2), activation="relu"))
+model.add(Convolution2D(36, 5, 5, subsample=(2, 2), activation="relu"))
+model.add(Convolution2D(48, 5, 5, subsample=(2, 2), activation="relu"))
+model.add(Convolution2D(64, 3, 3, activation="relu"))
 model.add(Convolution2D(64, 3, 3, activation="relu"))
 model.add(Flatten())
-model.add(Dense(200))
 model.add(Dense(100))
+model.add(Dense(50))
+model.add(Dense(20))
+model.add(Dense(10))
 model.add(Dense(1))
+'''
+#nvidia
+model = Sequential()
+model.add(Cropping2D(cropping=((70, 25), (0, 0)), input_shape=(160, 320, 3)))
+model.add(Convolution2D(24,5,5, subsample=(2,2), activation='relu'))
+model.add(Convolution2D(36,5,5, subsample=(2,2), activation='relu'))
+model.add(Convolution2D(48,5,5, subsample=(2,2), activation='relu'))
+model.add(Convolution2D(64,3,3, activation='relu'))
+model.add(Convolution2D(64,3,3, activation='relu'))
+model.add(Flatten())
+model.add(Dense(100))
+model.add(Dense(50))
+model.add(Dense(10))
+model.add(Dense(1))
+'''
 '''
 # lenet
 model = Sequential()
@@ -64,4 +85,4 @@ model.add(Dense(1))
 model.compile(loss='mse', optimizer='adam')
 model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=5)
 
-model.save('../mymodel.h5')
+model.save('../model_my16.h5')
