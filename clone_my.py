cc2eea269ba32dd52ad87330e2988ed7f34a9b4a
maanadoona <maanadoona@gmail.com>
2018-08-02 05:44:06 +0000

    more improved

--- clone_my.py
+++ clone_my.py
@@ -10,50 +10,56 @@ with open('../data/driving_log.csv') as csvfile:
 
 images = []
 measurements = []
-correction = 0.000001
+#correction = 0.000001
+correction = 0.1
 for line in lines:
 	for i in range(3):
+		source_path = line[i]
 		source_path = line[i]
 		filename = source_path.split('/')[-1]
 		current_path = '../data/IMG/' + filename
 		image = cv2.imread(current_path)
 		images.append(image)
+		if i == 0:
+			images.append(cv2.flip(image,1))
 	measurement = float(line[3])
 	measurement_left = measurement + correction
 	measurement_right = measurement - correction
 	measurements.append(measurement)
+	measurements.append(measurement*-1.0)
 	measurements.append(measurement_left)
+	#measurements.append(measurement_left*-1.0)
 	measurements.append(measurement_right)
-
+	#measurements.append(measurement_right*-1.0)
 
 X_train = np.array(images)
 y_train = np.array(measurements)
 
 
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Cropping2D
+from keras.layers import Flatten, Dense, Cropping2D, Dropout
 from keras.layers.core import Lambda
 from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
 
 X_train = np.array(X_train / 255.0 - 0.5)
 
-
 #my model
 model = Sequential()
 model.add(Cropping2D(cropping=((70, 25), (0, 0)), input_shape=(160, 320, 3)))
-model.add(Convolution2D(36, 5, 5, subsample=(2, 2), activation="relu"))
-model.add(Convolution2D(48, 5, 5, subsample=(2, 2), activation="relu"))
-model.add(Convolution2D(64, 5, 5, subsample=(2, 2), activation="relu"))
+model.add(Convolution2D(16, 5, 5, subsample=(2, 2), border_mode='valid', activation="relu"))
+model.add(Convolution2D(32, 5, 5, subsample=(2, 2), border_mode='valid', activation="relu"))
+model.add(Convolution2D(64, 5, 5, subsample=(2, 2), border_mode='valid', activation="relu"))
 model.add(Convolution2D(128, 3, 3, activation="relu"))
 model.add(Convolution2D(128, 3, 3, activation="relu"))
+model.add(Dropout(0.4))
 model.add(Flatten())
-model.add(Dense(100))
-model.add(Dense(50))
-model.add(Dense(20))
-model.add(Dense(10))
+model.add(Dense(100, activation='relu'))
+model.add(Dense(50, activation='relu'))
+model.add(Dense(10, activation='relu'))
 model.add(Dense(1))
 '''
+
 #nvidia
 model = Sequential()
 model.add(Cropping2D(cropping=((70, 25), (0, 0)), input_shape=(160, 320, 3)))
@@ -85,4 +91,4 @@ model.add(Dense(1))
 model.compile(loss='mse', optimizer='adam')
 model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=5)
 
-model.save('../model_my17.h5')
+model.save('../model_my24.h5')
